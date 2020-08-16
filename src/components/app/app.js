42c9a7448b693264b9d01f033464d4b52d9e4607
Itina Winter <lobanova.irina.96@mail.ru>
2020-08-16 21:34:14 +0300

    Начинает реализацию добавления задачи

---
+++ src/components/app/app.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+
+import AppHeader from '../app-header';
+import SearchPanel from '../search-panel';
+import TodoList from '../todo-list';
+import ItemStatusFilter from '../item-status-filter';
+import ItemAddForm from '../item-add-form';
+
+import './app.css';
+
+export default class App extends Component {
+
+  maxId = 100;
+
+  state = {
+    todoData: [
+      { label: 'Drink Coffee', important: false, id: 1 },
+      { label: 'Make Awesome App', important: true, id: 2 },
+      { label: 'Have a lunch', important: false, id: 3 }
+    ]
+  }
+
+  deleteItem = (id) => {
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex((el) => el.id === id);
+
+      const newArray = [
+        ...todoData.slice(0, idx),
+        ...todoData.slice(idx + 1)
+      ];
+
+      return {
+        todoData: newArray
+      };
+    });
+  };
+
+  addItem = (text) => {
+    const newItem = {
+      label: text,
+      important: false,
+      id: this.maxId++
+    };
+
+    this.setState( ({todoData}) => {
+      const newArray = [
+        ...todoData,
+        newItem
+      ];
+
+      return {
+        todoData: newArray
+      };
+    });
+  };
+
+  onToggleImportant = (id) => {
+    console.log('Toggle Important', id);
+  };
+
+  onToggleDone = (id) => {
+    console.log('Toggle Done', id);
+  };
+
+  render() {
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={ 1 } done={ 3 } />
+        <div className="top-palen d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+
+        <TodoList
+          todos={ this.state.todoData }
+          onDeleted={ this.deleteItem }
+          onToggleImportant = { this.onToggleImportant }
+          onToggleDone = { this.onToggleDone } />
+
+        <ItemAddForm onItemAdded={ this.addItem } /> 
+      </div>
+    );
+  };
+};
